971497a4633098b40fcf2366f328499f5080be64
cowsforyou <myrontayjt@gmail.com>
2015-10-11 17:59:28 -0400

    broken stuff

--- game/barracks_master/scripts/vscripts/barracks/bm_melee_barracks.lua
+++ game/barracks_master/scripts/vscripts/barracks/bm_melee_barracks.lua
@@ -2,7 +2,28 @@
 -- Melee Barracks AI
 --------------------------------------------------------------------------------------------------------
 function Spawn( keys )
-
-    SpawnCreeps("blue", 4, 30)
+    -- thisEntity is a special entity that references the unit who called this script
+    local playerID = thisEntity:GetMainControllingPlayer()
+    local color = GetPlayerColor(playerID)
+    SpawnCreeps(color, 4, 30)
     
 end
+
+function GetPlayerColor(playerID)
+    local player = PlayerResource:GetPlayer(playerID) -- converts the playerID index to a player handle
+    local unit = player:GetAssignedHero()
+    local unitName = unit:GetUnitName()
+    
+    local color = nil
+    if unitName == "npc_dota_hero_axe" then
+        color = "blue"
+    elseif unitName == "blah_blah" then
+        color = "whatever"
+    end
+    
+    return color
+end
+
+
+
+
