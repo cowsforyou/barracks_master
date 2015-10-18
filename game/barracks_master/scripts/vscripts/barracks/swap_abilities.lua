e50b779298911b942b65cebf077b9cbd3138363f
veggiesama <veggiesama@gmail.com>
2015-10-18 19:32:54 -0400

    Temp commit

---
+++ game/barracks_master/scripts/vscripts/barracks/swap_abilities.lua
@@ -0,0 +1,23 @@
+--------------------------------------------------------------------------------------------------------
+-- General spawner AI
+--------------------------------------------------------------------------------------------------------
+function OnSpellStart(keys)
+
+    local list1 = {
+        "bm_ranged_armor", 
+        "bm_ranged_armor", 
+        "bm_ranged_armor", 
+        "bm_ranged_armor", 
+        "bm_ranged_armor", 
+    }
+
+    local list2 = {
+        "bm_melee_armor",
+        "bm_melee_armor",
+        "bm_melee_armor",
+        "bm_melee_armor",
+        "bm_melee_armor",
+    }
+
+    
+end
