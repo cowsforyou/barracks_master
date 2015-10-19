b160caa36f1113254b5e20540af9c38e7002fd25
veggiesama <veggiesama@gmail.com>
2015-10-18 20:08:31 -0400

    Ability swap

--- game/barracks_master/scripts/vscripts/barracks/swap_abilities.lua
+++ game/barracks_master/scripts/vscripts/barracks/swap_abilities.lua
@@ -1,23 +1,41 @@
 --------------------------------------------------------------------------------------------------------
--- General spawner AI
+-- Swap abilities
 --------------------------------------------------------------------------------------------------------
 function OnSpellStart(keys)
+    local caster = keys.caster
 
-    local list1 = {
-        "bm_ranged_armor", 
-        "bm_ranged_armor", 
-        "bm_ranged_armor", 
-        "bm_ranged_armor", 
-        "bm_ranged_armor", 
-    }
+    local list_main = {}
+    list_main[0] = "repair"
+    list_main[1] = "build_bm_melee_barracks"
+    list_main[2] = "build_bm_siege_barracks"
+    list_main[3] = "build_bm_ranged_barracks"
+    list_main[4] = "build_bm_lumber_yard"
 
-    local list2 = {
-        "bm_melee_armor",
-        "bm_melee_armor",
-        "bm_melee_armor",
-        "bm_melee_armor",
-        "bm_melee_armor",
-    }
+    local list_alt = {}
+    list_alt[0] = "meepo_earthbind"
+    list_alt[1] = "meepo_poof"
+    list_alt[2] = "meepo_geostrike"
+    list_alt[3] = "treant_living_armor"
+    list_alt[4] = "treant_overgrowth"
 
-    
+    local list = {}
+    print(caster:GetAbilityByIndex(0):GetAbilityName())
+    print(list_main[0])
+    if caster:GetAbilityByIndex(0):GetAbilityName() == list_main[0] then
+        list = list_alt
+    else
+        list = list_main
+    end
+
+    for i=0,4 do
+        local ability = caster:GetAbilityByIndex(i)
+        local abilityName = ability:GetName()
+        caster:RemoveAbility(abilityName)
+
+        local newAbilityName = list[i]
+        caster:AddAbility(list[i])
+        local newAbility = caster:FindAbilityByName(newAbilityName)
+        newAbility:SetLevel(newAbility:GetMaxLevel())
+    end
+
 end
