9546340c49bc5656da5bc31ae708c7b662b97a9d
cowsforyou <myrontayjt@gmail.com>
2015-11-27 10:00:44 -0500

    autocast testing

--- game/barracks_master/scripts/vscripts/barracks/autocast.lua
+++ game/barracks_master/scripts/vscripts/barracks/autocast.lua
@@ -1,17 +1,10 @@
 --------------------------------------------------------------------------------------------------------
 -- Autocast: Makes an ability automatically autocast
 --------------------------------------------------------------------------------------------------------
-function Think( keys )
-    --print("Think")
-    local caster = keys.caster
+function Autocast( keys )
     local ability = keys.ability
 
     if ability.initialAutocast == nil and ability:GetAutoCastState() == false then
         ability:ToggleAutoCast()
-        ability.initialAutocast = true
-    end
-
-    if ability:GetAutoCastState() == true and ability:IsFullyCastable() and not caster:HasModifier("modifier_construction") then
-        caster:CastAbilityNoTarget(ability, 0)
     end
 end
