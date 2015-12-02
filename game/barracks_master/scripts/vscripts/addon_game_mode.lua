70951d5c55f88fd03c974e7602c201cd6141cb1b
cowsforyou <myrontayjt@gmail.com>
2015-12-02 00:59:30 -0500

    getdotastats addition

--- game/barracks_master/scripts/vscripts/addon_game_mode.lua
+++ game/barracks_master/scripts/vscripts/addon_game_mode.lua
@@ -12,6 +12,7 @@ require('orders')
 require('builder')
 require('buildinghelper')
 require('buildingevents') -- veg
+require('statcollection/init') -- cows
 
 require('barracks/spawner')
 require('barracks/spawn_synchronizer')
