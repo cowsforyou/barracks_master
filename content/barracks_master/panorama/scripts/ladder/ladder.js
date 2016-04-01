46eb78219e6bd44586e4067dd8b80a7e46e763b8
Martin Noya <martinnoya@gmail.com>
2016-04-01 20:47:49 -0300

    Updated ladder url and parse

--- content/barracks_master/panorama/scripts/ladder/ladder.js
+++ content/barracks_master/panorama/scripts/ladder/ladder.js
@@ -1,4 +1,4 @@
-var url = 'http://barracksmaster.com/api?key=UbiGPvDoFn7z8isRV6h71Mrq3q3N93N1&resource=ladder'
+var url = 'http://barracksmaster.herokuapp.com/top_10'
 
 function GetLadderData() {
     $.Msg("Getting Ladder data...")
@@ -7,16 +7,13 @@ function GetLadderData() {
             var ladder = JSON.parse(data);
             $.Msg("Recieved "+ladder.length+" entries!")
             for (var i in ladder) {
-                var rank = Number(i)+1
                 var player = ladder[i]
-                var name = player['steamdata']['personaname']
+                var rank = player['rnak']
+                var name = player['username']
                 var points = player['points']
-                var num_matches = player['num_matches']
-                var num_wins = player['num_wins']
-                var winrate = Math.floor(Number(num_wins) / Number(num_matches) * 100)
-                var steamid64 = player['steamid64']
-                $.Msg("Rank #"+rank+": "+name+" | Points: "+points+" | Matches: "+num_matches+" | Wins: "+num_wins+" ("+winrate+"% Winrate)")
-                CreateLadderPlayer(rank, steamid64, name)
+                var steamID = player['steamID']
+                $.Msg("Rank #"+rank+": "+name+" | Points: "+points)
+                CreateLadderPlayer(rank, steamID, name)
             };
         }
     })
